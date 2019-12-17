c328233ed16fdd60ba57fc724d0575a940851b03
weng lin <wenglin@buffalo.edu>
2019-12-17 04:47:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,5 +23,25 @@ def shift_with_args(array)
 end
 
 def using_concat(array)
-  array.concat(2)
+
+end
+
+def using_insert(array)
+
+end
+
+def using_uniq
+
+end
+
+def using_flatten
+
+end
+
+def using_delete
+
+end
+
+def using_delete_at(array,insteger)
+  array.delete(Integer)
 end
